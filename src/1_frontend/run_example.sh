6602b299d1133d404839e2e9ddf74277da344f1c
onestringlab <onestringlab@gmail.com>
2023-09-29 16:49:45 +0700

    All

--- src/1_frontend/run_example.sh
+++ src/1_frontend/run_example.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
 composer install
+
 cp .env.example .env
+
 php artisan key:generate
+
 chown -R $USER:www-data storage
+
 chown -R $USER:www-data bootstrap/cache
+
 chmod -R 775 storage
+
 chmod -R 775 bootstrap/cache
