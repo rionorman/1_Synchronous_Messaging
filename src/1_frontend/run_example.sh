6bcf11bed49b1bde7c4ded725b7f9cd0a2347fec
onestringlab <onestringlab@gmail.com>
2023-09-29 16:22:46 +0700

    all

--- src/1_frontend/run_example.sh
+++ src/1_frontend/run_example.sh
@@ -1,11 +1,12 @@
+#!/bin/sh
+
 composer install
 cp .env.example .env
 php artisan key:generate
-php artisan cache:clear
-php artisan config:clear
 sudo chown -R $USER:www-data storage
 sudo chown -R $USER:www-data bootstrap/cache
 chmod -R 775 storage
 chmod -R 775 bootstrap/cache
 npm install
 npm run dev
+npm run build
